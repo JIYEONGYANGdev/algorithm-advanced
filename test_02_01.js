7d249dd43fc1e7b2990ee2e2781ebf57b9d52078
JIYEONGYANGdev <sootulliyang.dev@gmail.com>
2021-12-11 18:23:37 +0900

    test_02-01 passed

--- test_02_01.js
+++ test_02_01.js
@@ -4,18 +4,16 @@ Implement a function that given a roman numeral as input, write a function that 
 **Constraints**: When a smaller numeral appears before a larger one, it becomes a subtractive operation. You can assume only one smaller numeral may appear in front of larger one.
 I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000
 1 <= romanNumeral <= 10,000
-
 */
 
+// test case all passed
 function test1(romanNumeral) {
-  // your code here
   const romanNums = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000
   }
-
   // 작은 단위의 로마숫자가 앞에 올 경우 - (minus)
   // 작은 단위의 로마숫자가 뒤에 올 경우 + (plus)
 
-  // 한 자리 인 경우 ?
+  // 한 자리인 경우 ?
   if (romanNumeral.length === 1) return romanNums[romanNumeral];
 
   // IV, VI < X
@@ -24,17 +22,24 @@ function test1(romanNumeral) {
   // 큰 단위 숫자 앞에 작은 단위는 한 번만 나옴
 
   // 완전 탐색 (두 자리 이상)
-  let result = 0;
-  for (let i = 0; i < romanNumeral.length; i++) {
-    if (romanNums[romanNumeral[i]] < romanNums[romanNumeral[i+1]]) {
-      result += romanNums[romanNumeral[i+1]] - romanNums[romanNumeral[i]]
+  // 누적 reduce ? 재귀?
+
+  let arr = romanNumeral.split('').map((roman) => romanNums[roman])
+
+  for (let i = arr.length -1; i >= 0; i--) {
+    if (i === arr.length-1) {
+      arr[i] = arr[i]
     }
-    else if (romanNums[romanNumeral[i]] > romanNums[romanNumeral[i + 1]]) {
-      result += romanNums[romanNumeral[i+1]] + romanNums[romanNumeral[i]]
+    else if (i >= 0) {
+      if (arr[i] < arr[i + 1]) {
+        arr[i] = - arr[i]
+      } else {
+        continue
+      }
     }
   }
-  return result;
 
+  return arr.reduce((acc,curr) => acc+curr, 0);
 }
 
 // **Examples**:
